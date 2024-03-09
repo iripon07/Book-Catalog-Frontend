d311acd5145b8bd9fb7fb26eb6ae70f30d63b512
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-03-09 15:03:51 +0600

    registration complete

--- src/component/RegistrationForm.tsx
+++ src/component/RegistrationForm.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useSignupMutation } from "../redux/features/user/userApi";
 interface ISignUpFormValues {
   name: string;
   email: string;
@@ -7,119 +11,134 @@ interface ISignUpFormValues {
 }
 
 const RegistrationForm = () => {
+  const navigate = useNavigate();
+  const [signup, { data, isError, isLoading, isSuccess }] = useSignupMutation();
   const {
     register,
     handleSubmit,
-    // reset,
+    reset,
     formState: { errors },
   } = useForm<ISignUpFormValues>();
 
   const onSubmit = (data: ISignUpFormValues) => {
+    signup({
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      address: data.address,
+    });
     console.log(data, "user information!");
   };
+  useEffect(() => {
+    if (!isLoading && !isError && isSuccess && data.statusCode === 200) {
+      toast.success(`Sign up successfully`);
+      reset();
+      navigate(`/login`);
+    } else if (!isLoading && isError) {
+      toast.error(`Sign up failed`);
+    }
+  }, [data, isError, isSuccess, navigate, reset, isLoading]);
 
   return (
     <>
-     
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex justify-center items-center mb-4">
-            <label className="form-control w-full max-w-xs">
-              <h6 className="text-xl text-gray-500">Name</h6>
-              <input
-                type="text"
-                id="name"
-                placeholder="Enter your name"
-                className="input input-bordered w-full max-w-xs"
-                {...register("name", {
-                  required: "Name is required",
-                })}
-              />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-center items-center mb-4">
+          <label className="form-control w-full max-w-xs">
+            <h6 className="text-xl text-gray-500">Name</h6>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+              className="input input-bordered w-full max-w-xs"
+              {...register("name", {
+                required: "Name is required",
+              })}
+            />
 
-              <div className="label">
-                {errors.name ? (
-                  <span className="text-red-900">
-                    {errors.name.message?.toString()}
-                  </span>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </label>
-          </div>
-
-          <div className="flex justify-center items-center mb-4">
-            <label className="form-control w-full max-w-xs">
-              <h6 className="text-xl text-gray-500">Email</h6>
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
-                className="input input-bordered w-full max-w-xs"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: /^\S+@\S+\.\S+$/,
-                })}
-              />
-              {errors?.email ? (
+            <div className="label">
+              {errors.name ? (
                 <span className="text-red-900">
-                  {errors?.email?.message?.toString()}
+                  {errors.name.message?.toString()}
                 </span>
               ) : (
                 <></>
               )}
-            </label>
-          </div>
+            </div>
+          </label>
+        </div>
 
-          <div className="flex justify-center items-center mb-4">
-            <label className="form-control w-full max-w-xs">
-              <h6 className="text-xl text-gray-500">Password</h6>
-              <input
-                type="password"
-                id="password"
-                placeholder="Enter your password"
-                className="input input-bordered w-full max-w-xs"
-                {...register("password", {
-                  required: "Password is required",
-                })}
-              />
-              {errors?.password ? (
-                <span className="text-red-900">
-                  {errors?.password?.message?.toString()}
-                </span>
-              ) : (
-                <></>
-              )}
-            </label>
-          </div>
+        <div className="flex justify-center items-center mb-4">
+          <label className="form-control w-full max-w-xs">
+            <h6 className="text-xl text-gray-500">Email</h6>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              className="input input-bordered w-full max-w-xs"
+              {...register("email", {
+                required: "Email is required",
+                pattern: /^\S+@\S+\.\S+$/,
+              })}
+            />
+            {errors?.email ? (
+              <span className="text-red-900">
+                {errors?.email?.message?.toString()}
+              </span>
+            ) : (
+              <></>
+            )}
+          </label>
+        </div>
 
-          <div className="flex justify-center items-center mb-4">
-            <label className="form-control w-full max-w-xs">
-              <h6 className="text-xl text-gray-500">Address</h6>
-              <input
-                type="text"
-                placeholder="Enter your password"
-                id="address"
-                className="input input-bordered w-full max-w-xs"
-                {...register("address", {
-                  required: "Address is required",
-                })}
-              />
-              {errors?.address ? (
-                <span className="text-red-900">
-                  {errors?.address?.message?.toString()}
-                </span>
-              ) : (
-                <></>
-              )}
-            </label>
-          </div>
-          <div className="card-actions flex justify-center items-center ">
-            <button className="btn btn-primary text-white text-xl uppercase width-full">
-              submit
-            </button>
-          </div>
-        </form>
-       
+        <div className="flex justify-center items-center mb-4">
+          <label className="form-control w-full max-w-xs">
+            <h6 className="text-xl text-gray-500">Password</h6>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              className="input input-bordered w-full max-w-xs"
+              {...register("password", {
+                required: "Password is required",
+              })}
+            />
+            {errors?.password ? (
+              <span className="text-red-900">
+                {errors?.password?.message?.toString()}
+              </span>
+            ) : (
+              <></>
+            )}
+          </label>
+        </div>
+
+        <div className="flex justify-center items-center mb-4">
+          <label className="form-control w-full max-w-xs">
+            <h6 className="text-xl text-gray-500">Address</h6>
+            <input
+              type="text"
+              placeholder="Enter your password"
+              id="address"
+              className="input input-bordered w-full max-w-xs"
+              {...register("address", {
+                required: "Address is required",
+              })}
+            />
+            {errors?.address ? (
+              <span className="text-red-900">
+                {errors?.address?.message?.toString()}
+              </span>
+            ) : (
+              <></>
+            )}
+          </label>
+        </div>
+        <div className="card-actions flex justify-center items-center ">
+          <button className="btn btn-primary text-white text-xl uppercase width-full">
+            submit
+          </button>
+        </div>
+      </form>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import routes from "./route/index.tsx";
 import store from "./redux/store.ts";
-import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer />
+      <Toaster position="top-center" reverseOrder={false} />
       <RouterProvider router={routes} />
     </Provider>
   </React.StrictMode>
